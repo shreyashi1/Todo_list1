757589e4d0139148c2f4121400b3cfa6de33d122
Shreyashi <coolshreyashi123@gmail.com>
2022-06-27 15:59:54 +0530

    Add gitignore, procfile and update ports

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://admin-shreyashi:Test123@cluster0.plqnf.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
@@ -143,6 +143,12 @@ app.get("/about", function(req, res){
 });
 
 
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
   console.log("Server has started successfully.");
 });
